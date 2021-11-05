00af10dac7941990036814fef5c017ffff36e7be
Ivanna0504 <guzovata17@gmail.com>
2021-11-05 20:31:00 +0200

    Study and training typeOfData JS

---
+++ typeOfData.js
@@ -0,0 +1,26 @@
+// //---01/11/2021---типи даних---змінні---перевірка типів даних---\\
+
+let firstName = "Ivanna";
+let lastName = "Huzovata";
+
+console.log(firstName + " " + lastName);
+
+let num = 1;
+let NUM = 1;
+
+let str = "Sone Srring";
+str = "Some String";
+console.log(typeof str);
+console.log(str);
+
+const boo = true;
+let n = null;
+let un;
+const x = {fName: "Ivanna", lName: "Huzovata" };
+
+console.log(typeof num);
+console.log(typeof NUM);
+console.log(typeof boo);
+console.log(typeof n);
+console.log(typeof un);
+console.log(typeof x);
