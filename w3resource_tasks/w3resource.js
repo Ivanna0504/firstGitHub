d27c7e411ffe58606bddac74b9fff50cdcc5904c
Ivanna Huzovata <ivanna.huzovata@globallogic.com>
2021-11-22 16:32:17 +0200

    study

--- w3resource_tasks/w3resource.js
+++ w3resource_tasks/w3resource.js
@@ -1,3 +1,17 @@
+//10  Calculate multiplication and division of two numbers
+
+const num1 = 12;
+const num2 = 10;
+const multiplication = num1 * num2;
+const division = num1/num2;
+
+console.log(multiplication, division);
+
+// Get a random integer from 1 to 10 inclusive
+const num = Math.ceil(Math.random() * 10);
+console.log(num);
+
+
 // 149 - JavaScript: Change the capitalization of all letters in a given string
 
 
